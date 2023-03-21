008e256c31bcda0656c38293a9e6153c07021de9
javayoyo <127707560+javayoyo@users.noreply.github.com>
2023-03-22 02:57:59 +0900

    CoinSongRoom re

---
+++ CoinSongRoom/src/CoinSongRoom/CoinSongRoomMain.java
@@ -0,0 +1,94 @@
+package CoinSongRoom;
+
+import java.util.Scanner;
+
+public class CoinSongRoomMain {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		CoinSongRoomService service = CoinSongRoomService.getInstance();
+		
+		boolean loginOk = false;
+		
+		while (true) {
+			System.out.println("=🎙=🎵=🎵=🎵=♬=♪=🎙=코 인 노 래 방=🎙=♪=♬=🎵=🎵=🎵=🎙=");
+
+			if (loginOk) {
+				System.out.println("--ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ--");
+				System.out.println("|  1.코인충전   2.가수검색    3.노래시작   4.사용내역       |");
+				System.out.println("|  5.추천노래 6.비밀번호수정 7.회원탈퇴  8.로그아웃  0.종료   |");
+				System.out.println("-ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ-");
+
+			} else {
+				System.out.println("-ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ-");
+				System.out.println("|  1.회원가입    2.로그인     3.리스트      0.종료       |");
+				System.out.println("-ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ-");
+			}
+
+			System.out.print("선택 > ");
+
+			int menu = sc.nextInt();
+
+			if (menu == 1) {
+				if (loginOk) {
+
+					service.coinCharge();
+
+				} else {
+					service.save();
+				}
+
+			}else if (menu == 2) {
+				if (loginOk) {
+					service.search();
+
+				} else {
+					loginOk = service.loginCheck();
+				}
+
+			}
+			else if (menu == 3) {
+
+				if (loginOk) {
+					service.singStart();
+
+				} else {
+					service.findAll();
+				}
+//			} else if (menu == 4 && loginOk) {
+//				service.findById();
+//
+//			} else if (menu == 5 && loginOk) {
+//				service.recommend();
+//
+			} else if (menu == 6 && loginOk) {
+				service.update();
+
+			} else if (menu == 7 && loginOk) {
+				service.delete();
+
+			} else if (menu == 8 && loginOk) {
+				service.logout();
+				loginOk = false;
+
+			} else if (menu == 0) {
+				
+				break;
+
+			} else {
+				System.out.println("다시입력");
+			}
+			System.out.println();
+		
+		System.out.println("프로그램 종료");
+	}
+		
+		
+	}
+
+
+	}
+
+
---
+++ CoinSongRoom/src/CoinSongRoom/CoinSongRoomService.java
@@ -0,0 +1,185 @@
+package CoinSongRoom;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class CoinSongRoomService {
+	
+	
+	private static CoinSongRoomService service = new CoinSongRoomService();
+	private CoinSongRoomService() {}
+	public static CoinSongRoomService getInstance() {
+		return service;
+	}
+	private Scanner sc = new Scanner(System.in);
+	private CoinSongRoomRepository repository = CoinSongRoomRepository.getInstance();
+	private String loginId = null;
+	private String loginPassword = null;
+	
+	public void save() {
+		CoinSongRoomDTO coinSongRoomDTO = new CoinSongRoomDTO();
+		
+		System.out.print("아이디 > ");
+		coinSongRoomDTO.setId(sc.next());
+
+		System.out.print("비밀번호 > ");
+		coinSongRoomDTO.setPassword(sc.next());
+		System.out.print("닉네임 > ");
+		coinSongRoomDTO.setNickname(sc.next());
+		repository.save(coinSongRoomDTO);
+		if (repository.save(coinSongRoomDTO)) {
+			System.out.println( "회원가입 성공");
+				
+		} else {
+			System.out.println("회원가입 실패");
+		}
+	
+	}
+	
+	public boolean loginCheck() {
+		System.out.print("아이디 > ");
+		String id = sc.next();
+		System.out.print("비밀번호 > ");
+		String password = sc.next();
+
+		if (repository.loginCheck(id, password)) { 
+			loginId = id;
+			loginPassword = password;
+			System.out.println("로그인성공");
+			return true;
+		} else {
+			System.out.println("로그인실패");
+			return false;
+		}
+	}
+	public void findAll() {
+		List<CoinSongRoomDTO> clist = repository.findAll();
+		System.out.println("회원번호\t아이디\t비밀번호\t닉네임\t잔액\t가입일");
+		System.out.println("-----------------------------------------------------------------");
+		for(CoinSongRoomDTO c : clist) {
+			System.out.println(c);
+		
+		}
+		
+	}
+	
+
+	
+	public void coinCharge() {
+		String cno = repository.getCno(loginId, loginPassword);
+		if(cno == null) {
+			System.out.println("로그인 오류");
+		}else {
+			System.out.println("충전금액 > ");
+			long money = sc.nextLong();
+			if(repository.coinCharge(cno, money)) {
+				System.out.println("충전 성공");
+			
+			}else {
+				System.out.println("충전 실패");
+			}
+	
+			
+		}
+	}
+	
+	
+	public void search() {
+		
+		System.out.println("가수 검색 > ");
+		String singer = sc.next();
+		
+		if(singer.equals("뉴진스")) {
+			System.out.println("20230102 뉴진스 - Ditto");
+		}else if(singer.equals("부석순")) {
+			System.out.println("20230206 부석순 - 파이팅해야지");
+		}else if(singer.equals("윤하")) {
+			System.out.println("20220330 윤하 - 오르트 구름");
+		}else if(singer.equals("아이브")) {
+			System.out.println("20220405 아이브 - LOVE DIVE");
+		}else if(singer.equals("아이들")) {
+			System.out.println("20221017 아이들 - Nxde");
+		}else {
+			System.out.println("조회할 수 없습니다. 다시 입력하세요. ");
+		
+		}
+	}
+	
+	public void singStart() {
+
+		System.out.println("번호 입력 > ");
+		int num = sc.nextInt();
+		
+		if(num ==20230102) {
+			System.out.println("┌ 20230102 뉴진스 - Ditto ┐");
+			System.out.println("🎙 : I got no time to lose / 내 길었던 하루 / 난 보고 싶어 / Ra-ta-ta-ta 울린 심장 (Ra-ta-ta-ta)");
+			
+			System.out.println("🎙 : I got nothing to lose / 널 좋아한다고 / wooah wooah wooah / Ra-ta-ta-ta 울린 심장 (Ra-ta-ta-ta)");
+			
+			
+		}else if(num ==20230206) {
+			System.out.println("┌ 20230206 부석순 - 파이팅해야지 ┐");
+			System.out.println("🎙 : 반복되는 하루에 / 시작이 되는 이 노래 / 네 옆에서 불러주겠어 / 힘내야지 뭐 어쩌겠어");
+			
+			System.out.println("🎙 : 파이팅 해야지 파이팅 해야지 / Don’t give it up Never give it up yeah / 파이팅 해야지 파이팅 해야지");
+			
+		}else if(num ==20220330) {
+			System.out.println("┌ 20220330 윤하 - 오르트 구름 ┐");
+			System.out.println("🎙 : Let’s go! / 새로운 길의 탐험가 / Beyond the road / 껍질을 깨뜨려버리자");
+			
+			System.out.println("🎙 : 두려움은 이제 거둬 / 오로지 나를 믿어 / 지금이 바로 time to fly");
+			
+		}else if(num ==20220405) {
+			System.out.println("┌ 20220405 아이브 - LOVE DIVE ┐");
+			System.out.println("🎙 : Narcissistic, my god I love it / 서로를 비춘 밤");
+			
+			System.out.println("🎙 : 아름다운 까만 눈빛 더 빠져 깊이 / (넌 내게로 난 네게로) / 숨 참고 love dive");
+			
+		}else if(num ==20221017) {
+			System.out.println("┌ 20221017 아이들 - Nxde ┐");
+			System.out.println("🎙 : Yes I’m a nude / Nude 따따랏따라 / Yes I’m a nude / Nude I don’t give a love ");
+			
+			System.out.println("🎙 : Baby how do I look, how do I look / 아리따운 날 입고 따따랏따라 ");
+			
+		}else {
+			System.out.println("조회할 수 없습니다. 다시 입력하세요.");
+		}
+	
+	
+	}
+	
+	public void update() {
+		System.out.println("비밀번호 확인 > ");
+		String password = sc.next();
+		if(password.equals(loginPassword)) {
+			System.out.println("수정할 비밃번호 > ");
+			String updatePassword = sc.next();
+			if(repository.update(loginId, loginPassword, updatePassword)) {
+				loginPassword = updatePassword;
+				System.out.println("업데이트 성공");
+			}else {
+				System.out.println("업데이트 실패");
+			}
+		}else {
+			System.out.println("비밀번호가 일치하지 않습니다");
+		}
+	}
+	public boolean delete() {
+		if(repository.delete(loginId, loginPassword)) {
+			System.out.println("삭제 완료");
+			return false;
+		}else {
+			System.out.println("삭제 실패");
+			return true;
+		}
+	}
+	public void logout() {
+		loginId = null;
+		loginPassword = null;
+		System.out.println("로그아웃");
+		
+	}
+	
+	
+
+}
